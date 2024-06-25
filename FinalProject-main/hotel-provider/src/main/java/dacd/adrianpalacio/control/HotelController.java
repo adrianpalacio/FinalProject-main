25c4cdc6a2709396a7fba1b391db46a57d3a0758
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-06-25 20:47:31 +0100

    Update HotelController.java
    minority changes

--- FinalProject-main/hotel-provider/src/main/java/dacd/adrianpalacio/control/HotelController.java
+++ FinalProject-main/hotel-provider/src/main/java/dacd/adrianpalacio/control/HotelController.java
@@ -1,1 +1,40 @@
+package dacd.adrianpalacio.control;
+
+import dacd.adrianpalacio.model.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+
+public class HotelController extends TimerTask {
+    private final HotelSupplier tripadvisorHotelSupplier;
+    private final HotelStore jmsHotelStore;
+
+    public HotelController(HotelSupplier tripadvisorHotelSupplier, HotelStore jmsHotelStore) {
+        this.tripadvisorHotelSupplier = tripadvisorHotelSupplier;
+        this.jmsHotelStore = jmsHotelStore;
+    }
+
+    public void execute(){
+        List<String> places = new ArrayList<>();
+        places.add("La-Graciosa");
+        places.add("Lanzarote");
+        places.add("Fuerteventura");
+        places.add("Gran-Canaria");
+        places.add("Tenerife");
+        places.add("La-Gomera");
+        places.add("La-Palma");
+        places.add("El-Hierro");
+
+        for (String place : places) {
+            List<Hotel> hotels = tripadvisorHotelSupplier.get(place);
+            jmsHotelStore.save(hotels);
+        }
+        System.out.println("-----[MESSAGE]: ALL DATA SENT.-----");
+    }
+
+    public void run(){
+        execute();
+    }
+
+}
 
